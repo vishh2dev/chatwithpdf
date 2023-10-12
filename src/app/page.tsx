fb9cf0f91f9bb92bd92a0874ea65ca6635fcaca9
vishh2dev <vishh2dev@gmail.com>
2023-10-12 23:53:16 +0530

    fix(landingpage):fixed some typo error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import {ArrowRight} from 'lucide-react'
 import { buttonVariants } from "@/components/ui/button";
 export default function Home() {
   return (
-   <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex-col item-center justify-center text-center ">
+   <MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
     <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
       <p className="text-sm font-semibold text-gray-700">
         Chatwith pdf
