6e1f6ed199904deae00ee4a5e289ff2ee373163d
vishh2dev <vishh2dev@gmail.com>
2023-12-06 23:44:07 +0530

    feat:dashbord created

---
+++ src/components/UploadButton.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React, { useState } from 'react'
+
+const UploadButton = () => {
+    const[isOpen,setIsOpen] = useState<boolean>(false)
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UploadButton
