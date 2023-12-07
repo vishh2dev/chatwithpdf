582ed176327f9ddf07b8f85e52a65f3174fadb23
vishh2dev <vishh2dev@gmail.com>
2023-12-07 23:05:56 +0530

    feat:upload added

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,13 +1,24 @@
 "use client"
 import React, { useState } from 'react'
+import { Dialog, DialogContent, DialogTrigger } from './ui/dialog'
+import { Button } from './ui/button'
 
 const UploadButton = () => {
     const[isOpen,setIsOpen] = useState<boolean>(false)
 
   return (
-    <div>
-      
-    </div>
+    <Dialog open={isOpen} onOpenChange={(v) =>{
+      if(!v){
+        setIsOpen(v)
+      }
+    }}>
+      <DialogTrigger asChild onClick={()=>{setIsOpen(true)}}>
+        <Button>Upload pdf</Button>
+      </DialogTrigger>
+      <DialogContent>
+        examplwecontent
+      </DialogContent>
+    </Dialog>
   )
 }
 
